239bc48a9043deef322a9633d785065bb2b4a5bb
GlCap <glmcaputo@gmail.com>
2021-04-02 17:27:32 +0200

    feat: class rework, errors, compare methods - make parse method static - use parse method to parse string internally - added InvalidStringError - overload construction declaration - added number to Day, indicates which day of the week the instance is representing

--- src/types.ts
+++ src/types.ts
@@ -8,16 +8,19 @@ export interface RangeSerializable {
   end: TimeSerializable;
 }
 
+export type WeekNumbers = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
 export interface DaySerializable {
+  number: WeekNumbers | -1;
   ranges: RangeSerializable[];
 }
 
 export interface WeekSerializable {
-  monday: DaySerializable;
-  tuesday: DaySerializable;
-  wednesday: DaySerializable;
-  thursday: DaySerializable;
-  friday: DaySerializable;
-  saturday: DaySerializable;
-  sunday: DaySerializable;
+  monday: DaySerializable | null;
+  tuesday: DaySerializable | null;
+  wednesday: DaySerializable | null;
+  thursday: DaySerializable | null;
+  friday: DaySerializable | null;
+  saturday: DaySerializable | null;
+  sunday: DaySerializable | null;
 }
