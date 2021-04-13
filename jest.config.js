10164cf99405519ae0af9909a342480be5501f65
GlCap <glmcaputo@gmail.com>
2021-04-13 15:16:50 +0200

    chore: test config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  coverageDirectory: './coverage',
+  collectCoverage: true,
+  coverageReporters: ['html', 'json', 'lcov', 'text', 'clover'],
 };
