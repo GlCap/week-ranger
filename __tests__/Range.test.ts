0ba37d7169bfd0e83080042514095b64e6cdb165
GlCap <glmcaputo@gmail.com>
2021-04-13 13:32:48 +0200

    test: reorganized Range tests

--- __tests__/Range.test.ts
+++ __tests__/Range.test.ts
@@ -3,56 +3,73 @@ import { Range } from '../src/core/Range';
 describe('Range class', () => {
   const rangeString = '08:30-10:05';
 
+  const rangeAfterString = '10:05-11:55';
+  const rangeBeforeString = '05:00-08:30';
+  const rangeSameStartString = '08:30-10:00';
+
   const range = new Range(rangeString);
+  const rangeAfter = new Range(rangeAfterString);
+  const rangeBefore = new Range(rangeBeforeString);
+  const rangeSameEnd = new Range(rangeSameStartString);
 
-  it('can be parsed from a string', () => {
-    const rangeError = '08:30-07:30';
-    const rangeErrorShort = '08:3007:30';
-
-    expect(Range.parse(rangeString)).toBeDefined();
-    expect(() => Range.parse(rangeErrorShort)).toThrow();
-    expect(() => Range.parse(rangeError)).toThrow();
+  describe('getters', () => {
+    test('start should be before end', () => {
+      expect(range.end.isAfter(range.start)).toBe(true);
+      expect(range.start.isAfter(range.end)).toBe(false);
+    });
   });
 
-  it('can be created from another Range', () => {
-    expect(new Range(range)).toBeDefined();
-    expect(range).toStrictEqual(new Range(range));
+  describe('constructor', () => {
+    it('can be created from another Range', () => {
+      expect(new Range(range)).toBeDefined();
+      expect(range).toStrictEqual(new Range(range));
+    });
   });
 
-  it('start must be before end', () => {
-    expect(range.end.isAfter(range.start)).toBeTruthy();
-    expect(range.start.isAfter(range.end)).toBeFalsy();
+  describe('parse', () => {
+    it.each([rangeString, rangeAfterString, rangeBeforeString, rangeSameStartString])(
+      'should parse a formatted string',
+      (range) => {
+        expect(Range.parse(range)).toBeDefined();
+      },
+    );
+
+    it('should throw on invalid string', () => {
+      const rangeError = '08:30-07:30';
+      const rangeErrorShort = '08:3007:30';
+
+      expect(() => Range.parse(rangeErrorShort)).toThrow();
+      expect(() => Range.parse(rangeError)).toThrow();
+    });
   });
 
-  it('can be serialized to a parsable string', () => {
-    expect(range.toString()).toStrictEqual(rangeString);
-    expect(new Range(range.toString())).toStrictEqual(range);
+  describe('toString', () => {
+    it('should serialize to a parsable string', () => {
+      expect(range.toString()).toStrictEqual(rangeString);
+      expect(new Range(range.toString())).toStrictEqual(range);
+    });
   });
 
-  it('can be serialized to a tuple of dates', () => {
-    const [start, end] = range.toDate();
-    const [startFrom, endFrom] = range.toDate(new Date(0));
-    expect(range.toDate()).toBeDefined();
-    expect(start instanceof Date && end instanceof Date).toBeTruthy();
-    expect(startFrom instanceof Date && endFrom instanceof Date).toBeTruthy();
-    expect(start.getTime() <= end.getTime()).toBeTruthy();
-    expect(startFrom.getTime() <= endFrom.getTime()).toBeTruthy();
+  describe('toDate', () => {
+    it('should serialize to a tuple of dates', () => {
+      const [start, end] = range.toDate();
+      const [startFrom, endFrom] = range.toDate(new Date(0));
+      expect(range.toDate()).toBeDefined();
+      expect(start instanceof Date && end instanceof Date).toBeTruthy();
+      expect(startFrom instanceof Date && endFrom instanceof Date).toBeTruthy();
+      expect(start.getTime() <= end.getTime()).toBeTruthy();
+      expect(startFrom.getTime() <= endFrom.getTime()).toBeTruthy();
+    });
   });
 
-  it('can be serialized to JSON', () => {
-    expect(JSON.parse(JSON.stringify(range))).toStrictEqual(range.toJSON());
-    expect(new Range(range.toJSON())).toStrictEqual(range);
+  describe('toJSON', () => {
+    it('should serialize to JSON', () => {
+      expect(JSON.parse(JSON.stringify(range))).toStrictEqual(range.toJSON());
+      expect(new Range(range.toJSON())).toStrictEqual(range);
+    });
   });
 
-  it('can be compared', () => {
-    const rangeAfterString = '10:05-11:55';
-    const rangeBeforeString = '05:00-08:30';
-    const rangeSameStartString = '08:30-10:00';
-
-    const rangeAfter = new Range(rangeAfterString);
-    const rangeBefore = new Range(rangeBeforeString);
-    const rangeSameEnd = new Range(rangeSameStartString);
-
+  describe('compareTo', () => {
     expect(range.compareTo(range)).toBe(0);
     expect(range.compareTo(rangeAfter)).toBeLessThan(0);
     expect(rangeAfter.compareTo(range)).toBeGreaterThan(0);
@@ -62,16 +79,32 @@ describe('Range class', () => {
 
     expect(range.compareTo(rangeSameEnd)).toBeGreaterThan(0);
     expect(rangeSameEnd.compareTo(range)).toBeLessThan(0);
+  });
 
-    expect(range.equals(range)).toBeTruthy();
-    expect(range.equals(rangeAfter)).toBeFalsy();
-    expect(range.equals(rangeBefore)).toBeFalsy();
-    expect(range.equals(rangeSameEnd)).toBeFalsy();
+  describe('equals', () => {
+    it('should be true', () => {
+      expect(range.equals(range)).toBeTruthy();
+      expect(rangeAfter.equals(rangeAfter)).toBeTruthy();
+      expect(rangeBefore.equals(rangeBefore)).toBeTruthy();
+      expect(rangeSameEnd.equals(rangeSameEnd)).toBeTruthy();
+    });
 
-    expect(rangeAfter.isAfter(range)).toBeTruthy();
-    expect(range.isAfter(rangeAfter)).toBeFalsy();
+    it('should be false', () => {
+      expect(range.equals(rangeAfter)).toBeFalsy();
+      expect(range.equals(rangeBefore)).toBeFalsy();
+      expect(range.equals(rangeSameEnd)).toBeFalsy();
+    });
+  });
 
-    expect(range.isAfter(rangeBefore)).toBeTruthy();
-    expect(rangeBefore.isAfter(range)).toBeFalsy();
+  describe('isAfter', () => {
+    it('should be after', () => {
+      expect(rangeAfter.isAfter(range)).toBeTruthy();
+      expect(range.isAfter(rangeBefore)).toBeTruthy();
+    });
+
+    it('should not be after', () => {
+      expect(range.isAfter(rangeAfter)).toBeFalsy();
+      expect(rangeBefore.isAfter(range)).toBeFalsy();
+    });
   });
 });
